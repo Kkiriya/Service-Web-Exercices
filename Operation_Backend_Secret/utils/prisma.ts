faed84d9b48be826fe0e4a5af778401f1b89ee0a
Émile <69686396+Kkiriya@users.noreply.github.com>
2026-06-25 10:47:28 -0400

    finished schema.prisma, prisma.ts and prisma.config.ts, connection is now working

--- Operation_Backend_Secret/utils/prisma.ts
+++ Operation_Backend_Secret/utils/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaNeon } from "@prisma/adapter-neon";
-import { PrismaClient } from "../generated/prisma/schema.prisma";
+import { PrismaClient } from "../generated/prisma/client";
 import dotenv from "dotenv";
 import { log } from "node:console";
 
